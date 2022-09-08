0e4e38047dba16160380be3ea641d78548445cba
CAMPUS\venkata.ramana <venkatar@interfile.co.za>
2022-09-08 12:57:43 +0200

    test1

--- src/main/java/dockerjenkinsintegration.java
+++ src/main/java/dockerjenkinsintegration.java
@@ -1,6 +1,10 @@
+import java.util.Arrays;
+
 public class dockerjenkinsintegration {
     public static void main(String[] args) {
+        //test1
         System.out.println("CICD-Integration with Docker and Jenkins");
+
 
     }
 }
